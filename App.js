302a22143275b84d478e2c6d18d6c8844cabf22d
Eka Desyantoro <haloekades@gmail.com>
2020-03-02 12:48:33 +0700

    step 3 - Foldering add screen login, splash, home

--- App.js
+++ App.js
@@ -2,14 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import Splash from '../rn_2020/src/screen/splash'
 
 const Stack = createStackNavigator();
 
@@ -17,7 +10,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Splash} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screen/login/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function LoginScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+  
+export default LoginScreen;
