8c5ec6027786b00c6aff24be746e1ec152f6b2c1
Eka Desyantoro <haloekades@gmail.com>
2020-05-12 16:55:30 +0700

    change apps to src

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleProvider, Root } from "native-base";
-import { Splash, LoginForm} from "./apps/screens";
-import getTheme from './apps/theme/components';
-import color from './apps/theme/variables/myColor';
+import { Splash, LoginForm} from "./src/screens";
+import getTheme from './src/theme/components';
+import color from './src/theme/variables/myColor';
 
 const StackApp = createStackNavigator();
 
